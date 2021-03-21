848d04bc1b77e8e1ae6974353fe647a35a9f7f61
kurooooon <b1006110@gmail.com>
2021-03-21 13:28:26 +0900

    apply serverless nextjs component

---
+++ .serverless_nextjs/assets/_next/static/niGi7gz1aewDmwWHSAz50/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002Fc50ddf22b716c7b6b76d.css","static\u002Fchunks\u002Fpages\u002Findex-f8ac0a3737b579abfb67.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-a877aeb767fe4a2952aa.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
